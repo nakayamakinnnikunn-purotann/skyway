a125110cc4d73a6aa59063e99ee8109257a8b15b
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-08-07 17:24:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -52,29 +52,12 @@ $(function() {
   const videoSelect = $('#videoSource');
   const selectors = [audioSelect, videoSelect];
 
-  navigator.mediaDevices.enumerateDevices()
-    .then(deviceInfos => {
-      const values = selectors.map(select => select.val() || '');
-      selectors.forEach(select => {
-        const children = select.children(':first');
-        while (children.length) {
-          select.remove(children);
-        }
-      });
+
 
       for (let i = 0; i !== deviceInfos.length; ++i) {
         const deviceInfo = deviceInfos[i];
         const option = $('<option>').val(deviceInfo.deviceId);
 
-        if (deviceInfo.kind === 'audioinput') {
-          option.text(deviceInfo.label ||
-            'Microphone ' + (audioSelect.children().length + 1));
-          audioSelect.append(option);
-        } else if (deviceInfo.kind === 'videoinput') {
-          option.text(deviceInfo.label ||
-            'Camera ' + (videoSelect.children().length + 1));
-          videoSelect.append(option);
-        }
       }
 
       selectors.forEach((select, selectorIndex) => {
